8d657c1fadd33c1499626cf6152fff98288d0613
fater2 <56210266+fater2@users.noreply.github.com>
2021-02-19 23:36:18 +0200

    Initial commit

---
+++ SupplierAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Supplier.Interfaces;
+using Supplier.Models;
+
+namespace SupplierAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _repository;
+        //private readonly IMapper _mapper;
+        public ProductsController(IGenericRepository<Product> repository)
+        {
+            //_mapper = mapper;
+            this._repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            var items = _repository.List();
+            return Ok(items);
+
+        }
+    }
+}
